76950778df48b682acd6f99a513b58f3aea4255c
abdelraouf <rofarewsh@gmail.com>
2016-11-29 21:04:42 +0200

    update EventApplication component

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161129175410) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.date     "event_date"
+    t.text     "description"
+    t.string   "place"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
